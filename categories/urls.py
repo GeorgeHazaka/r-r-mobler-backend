91b44f72d46975d600cbdffdbefaf2fd745bb112
GeorgeHazaka <hazaka.george@gmail.com>
2024-06-17 16:28:07 +0000

    Add CategoryList and CategoryDetail views to categories

--- categories/urls.py
+++ categories/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from categories import views
 
 urlpatterns = [
-  path('categories/', views.CategoryList.as_view()),
+    path('categories/', views.CategoryList.as_view(), name='category-list'),
+    path('categories/<int:pk>/', views.CategoryDetail.as_view(), name='category-detail'),
 ]
--- categories/views.py
+++ categories/views.py
@@ -1,11 +1,59 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAdminUser
 from .models import Category
 from .serializers import CategorySerializer
 
-
 class CategoryList(APIView):
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminUser]
+
     def get(self, request):
         categories = Category.objects.all()
-        serializer = CategorySerializer(categories, many=True)
+        serializer = CategorySerializer(
+            categories, many=True, context={'request': request}
+        )
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CategorySerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CategoryDetail(APIView):
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminUser]
+
+    def get_object(self, pk):
+        try:
+            return Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        category = self.get_object(pk)
+        serializer = CategorySerializer(
+            category, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        category = self.get_object(pk)
+        serializer = CategorySerializer(
+            category, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        category = self.get_object(pk)
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
