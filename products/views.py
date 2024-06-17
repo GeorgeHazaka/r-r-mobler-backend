6ea4723e79587faf2fdf6a61f70a2b4d5a9a17b2
GeorgeHazaka <hazaka.george@gmail.com>
2024-06-17 15:49:56 +0000

    Add ProductList and ProductDetail views to products

--- products/views.py
+++ products/views.py
@@ -1,11 +1,69 @@
+from django.http import Http404
+from rest_framework import permissions
+from rrmobler.permissions import IsAdminUserOrReadOnly
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
 
 
 class ProductList(APIView):
+    serializer_class = ProductSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
+
     def get(self, request):
         products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
+        serializer = ProductSerializer(
+            products, many=True, context={'request': request}
+        )
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data, context={'request': request}
+    )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProductDetail(APIView):
+    serializer_class = ProductSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
+
+    def get_object(self, pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        try:
+            product = self.get_object(pk)
+            serializer = ProductSerializer(
+                product, context={'request': request}
+            )
+            return Response(serializer.data)
+        except Product.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, pk):
+        try:
+            product = self.get_object(pk)
+            serializer = ProductSerializer(
+                product, data=request.data, context={'request': request}
+            )
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Product.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def delete(self, request, pk):
+        try:
+            product = self.get_object(pk)
+            product.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Product.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
--- rrmobler/permissions.py
+++ rrmobler/permissions.py
@@ -6,3 +6,17 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     if request.method in permissions.SAFE_METHODS:
       return True
     return obj.owner == request.user
+
+
+class IsAdminUserOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow admin users to create, update, or delete a product.
+    """
+
+    def has_permission(self, request, view):
+        # Allow read-only access to any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Only allow write access to admin users
+        return request.user and request.user.is_staff
