604ef0f74fa460d399ff554fb692293b41d440af
GeorgeHazaka <hazaka.george@gmail.com>
2024-06-23 18:33:06 +0000

    Add views to the route page

--- rrmobler/settings.py
+++ rrmobler/settings.py
@@ -23,6 +23,15 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+REST_FRAMEWORK = {
+    'DATETIME_FORMAT': '%d %b %Y',
+}
+
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -33,7 +42,7 @@ SECRET_KEY = 'django-insecure-c$(#6_86&*0u#oo$uj&jijk8f0)9u9ejhpqhk@$!l69l)$%)_(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-georgehazak-rrmoblerbac-md9llem6hnl.ws-eu114.gitpod.io']
+ALLOWED_HOSTS = ['8000-georgehazak-rrmoblerbac-zrtly61frwa.ws-eu114.gitpod.io']
 
 
 # Application definition
