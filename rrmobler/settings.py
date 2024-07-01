40c8f1b35774c93b808410956ef10c4f5e3c0ab3
GeorgeHazaka <hazaka.george@gmail.com>
2024-07-01 16:00:46 +0000

    Deployment

--- rrmobler/settings.py
+++ rrmobler/settings.py
@@ -40,6 +40,7 @@ REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'rrmobler.serializers.CurrentUserSerializer'
@@ -55,12 +56,16 @@ if 'DEV' not in os.environ:
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c$(#6_86&*0u#oo$uj&jijk8f0)9u9ejhpqhk@$!l69l)$%)_('
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-georgehazak-rrmoblerbac-z7mpwbg5w5j.ws-eu114.gitpod.io']
+ALLOWED_HOSTS = [
+    '8000-georgehazak-rrmoblerbac-z7mpwbg5w5j.ws-eu114.gitpod.io',
+    'localhost',
+    'rrmobler.herokuapp.com',
+]
 
 
 # Application definition
@@ -77,6 +82,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'dj_rest_auth',
+    'corsheaders',
 
     'profiles',
     'categories',
@@ -91,6 +97,7 @@ INSTALLED_APPS = [
 ]
 SITE_ID = 1
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -99,6 +106,17 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+else:
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r"^https://.*\.gitpod\.io$",
+    ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'rrmobler.urls'
 
